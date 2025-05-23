fb46052e90c952b2c007fb26a804636d74c94145
arielveitsman <arieshorts@gmail.com>
2025-05-23 23:32:26 +0300

    Добавлено 2 новых функций в TOML как divide, substract для тестов

--- mono/m_test.d
+++ mono/m_test.d
@@ -10,6 +10,14 @@ extern(C) int multiply(const int a, const int b) nothrow @nogc
     return a * b;
 }
 
+extern(C) int subtract(const int a, const int b) nothrow @nogc {
+    return a - b;
+}
+
+extern(C) int divide(const int a, const int b) nothrow @nogc {
+    return b == 0 ? 0 : a / b;
+}
+
 extern(C) void echo(const char* str) nothrow
 {
     import core.stdc.stdio : printf;
